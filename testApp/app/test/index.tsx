f7b7bbef8e5be024ef3c6932e7f72c868aefd684
swapnil ingle <ingleswapnil2004@gmail.com>
2025-01-26 03:02:44 +0530

    test app dev

---
+++ testApp/app/test/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const testIndex  = () => {
+    return (
+        <View
+            className='h-screen flex justify-center items-center bg-gray-100'
+            
+        >
+            <Text
+            className='text-2xl font-bold text-white'
+            >index</Text>
+        </View>
+    )
+}
+
+export default  testIndex
